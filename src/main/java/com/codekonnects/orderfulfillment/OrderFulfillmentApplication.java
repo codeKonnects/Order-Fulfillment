72411085236266c0c9abdb59edfa14340ba3db5d
codeKonnects <topedaves@gmail.co>
2022-10-15 02:09:54 +0100

    set up order fulfillment system

---
+++ src/main/java/com/codekonnects/orderfulfillment/OrderFulfillmentApplication.java
@@ -0,0 +1,13 @@
+package com.codekonnects.orderfulfillment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFulfillmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderFulfillmentApplication.class, args);
+    }
+
+}
